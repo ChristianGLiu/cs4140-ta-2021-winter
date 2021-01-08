5c199fc9d2adc60bb310d7984ae2c486a0e689f6
Chris Gang Liu <10833666+chrisgangliu@users.noreply.github.com>
2021-01-07 23:10:05 -0400

    Update database.php

--- database.php
+++ database.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'db.cs.dal.ca');
-define('DB_USERNAME', 'gang');
-define('DB_PASSWORD', '780309vY');
-define('DB_NAME', 'gang');
+define('DB_USERNAME', '***');
+define('DB_PASSWORD', '****');
+define('DB_NAME', '****');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
